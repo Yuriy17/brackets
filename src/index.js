c0f1261411fce8f06468e3d3944a6864cdc17c07
Yuriy17 <yuriy_sh@zoho.com>
2019-09-26 03:13:51 +0300

    brackets/feat:realize simple comparing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let x, y, z, num_open=0,num_closed=0,open=[],close=[];
+  for (let j = 0; j < str.length; j++) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if(str[j]==bracketsConfig[i][0]){
+            /*open.push(bracketsConfig[i][0]);*/
+            num_open++;
+        }
+        if(str[j]==bracketsConfig[i][1]&&num_open>num_closed){num_closed++;}
+    }   
+  }
+  if(num_open==num_closed){
+    return true;
+  }else{
+    return false;
+  }
 }
