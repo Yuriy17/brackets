299fb42fe5e6a2f40eda38682a674ad892b46186
Yuriy17 <yuriy_sh@zoho.com>
2019-09-27 21:45:41 +0300

    brackets/feat: pass all tests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  let x, y, z, num_open=0,num_closed=0,open=[],close=[];
+
+  let open=[];
   for (let j = 0; j < str.length; j++) {
     for (let i = 0; i < bracketsConfig.length; i++) {
-        if(str[j]==bracketsConfig[i][0]){
-            /*open.push(bracketsConfig[i][0]);*/
-            num_open++;
+      if(str[j]==bracketsConfig[i][0]){
+        if (!(bracketsConfig[i][0]==bracketsConfig[i][1] && open.includes(i))){
+          open.push(i);
+          break;
         }
-        if(str[j]==bracketsConfig[i][1]&&num_open>num_closed){num_closed++;}
-    }   
+      }
+      if(str[j]==bracketsConfig[i][1]){
+        if (open[open.length - 1]==i) {
+          open.pop();
+          break;
+        }
+      }
+      if(str[j]==bracketsConfig[i][1]&&!open.includes(i)){
+        return false;
+      }      
+
+    } 
   }
-  if(num_open==num_closed){
-    return true;
-  }else{
+
+  if(open.length>0){
     return false;
   }
+  return true;
 }
